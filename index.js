420e66404eeaf07eebce1afefe6b493c12e647a2
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-22 01:26:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function createPost() {
-    let postAuthor = document.getElementById('postAuthor').value;
-    let postTitle = document.getElementById('postTitle').value;
-    let postBody = document.getElementById('postBody').value;
-    let postTemplate = document.getElementById('post-template').innerHTML;
-    let postTemplateFn = _.template(postTemplate);
-    let postTemplateHTML = postTemplateFn({ postAuthor: postAuthor, postTitle: postTitle, postBody: postBody })
-
+function createPost(){
+  let  postAuthor = document.getElementById('postAuthor').value;
+  let postTitle = document.getElementById('postTitle').value;
+ let postBody = document.getElementById('postBody').value;
+ let postTemplate = document.getElementById('post-template').innerHTML;
+ let postTemplateFn = _.template(postTemplate);
+ let postTemplateHTML = postTemplateFn({ postAuthor: postAuthor, postTitle: postTitle, postBody: postBody })
+};
+function postComment() {
 };
 
-function postComment(post) {
+function createPage(post) {
 let pageTemplate = document.getElementById('page-template').innerHTML;
-let pageTemplateFn = _.template(pageTemplate);
-let main = document.querySelector('main');
-let pageTemplateHTML = pageTemplateFn({ newPost: post });
-main.innerHTML += pageTemplateHTML;
-};
+ let pageTemplateFn = _.template(pageTemplate);
+ let main = document.querySelector('main');
+ let pageTemplateHTML = pageTemplateFn({ newPost: post });
+ main.innerHTML += pageTemplateHTML;
+}; 
